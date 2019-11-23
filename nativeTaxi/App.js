c0dcbd67e6b41e45c448f66198d9dcbb4d4dd682
Lisandro Gomez <draivenl@gmail.com>
2019-11-23 11:34:20 -0500

    getting a first location withreact-native-geolocation-service, adding permissions

--- nativeTaxi/App.js
+++ nativeTaxi/App.js
@@ -1,31 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   Text,
   StatusBar,
-  View
+  View,
+  PermissionsAndroid
 } from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 6.2079964,
-        longitude: -75.5686769,
-        latitudeDelta: 0.0015,
-        longitudeDelta: 0.0121,
-      }}
-    >
-    </MapView>
-  </View>
-  );
+class App extends Component {
+  constructor( props ){
+    super(props)
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      error: null
+    }
+  }
+
+
+  async requestCameraPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'NativeTaxi Location App Permission',
+          message:
+            'NativeTaxi needs access to your location ' +
+            'to know your current position.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the locatio');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  componentDidMount(){
+    this.requestCameraPermission()
+    Geolocation.getCurrentPosition(
+        (position) => {
+            console.log(position.coords);
+            
+            this.setState({
+              longitude: position.coords.longitude,
+              latitude: position.coords.latitude
+            })
+        },
+        (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+        },
+        { enableHighAccuracy: true, timeout: 10000, maximumAge: 1 }
+    );
+    
+    
+    //navigator.geolocation.getCurrentPosition()
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: this.state.latitude,
+          longitude: this.state.longitude,
+          latitudeDelta: 0.0015,
+          longitudeDelta: 0.0121,
+        }}
+      />
+      <Marker
+        coordinate={this.state}
+        pinColor='red'
+        
+      >
+      </Marker>
+    </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
